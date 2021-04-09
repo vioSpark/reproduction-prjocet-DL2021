d93ac22b943008e80b59f12c333fc9273c71cc87
Mark <40240957+vioSpark@users.noreply.github.com>
2021-04-09 12:43:40 +0200

    Implementing T->H (#3)
    * bases are partly done
    
    * pictures added
    
    * bases added
    
    * first set of errors debugged
    
    * gradient of T->H layer is working
    
    Co-authored-by: lukacs.mark@gmail.com <vioSpark@users.noreply.github.com>

--- SESN_parts.py
+++ SESN_parts.py
@@ -1,7 +1,7 @@
 import torch
 import math
 import numpy as np
-from scipy.special import hermite
+from scipy.special import eval_hermite
 
 """
     Equation 7
@@ -28,28 +28,32 @@ class convTH:
         self.V = V
         self.S = S
         self.Nb = Nb
-        # bases have the shape [Nb, S, bases_size, bases_size]
-        # TODO: hermite bases
-        self.bases = hermite_bases()
+        # bases have the shape [Nb, S, V, V]
+        self.bases = hermite_bases(Nb, S, V)
 
         # weights
         # w is an array of shape [Cout, Cin, Nb]
         # Nb is the number of steerable functions we represent a filter with
         # To say a metaphor, the bigger Nb, the more terms we've fetched in a Taylor approximation ..
         #       if this is chinese, ask Mark about it
-        self.weights = torch.Tensor(self.Cout, self.Cin, Nb)
+        self.weights = torch.Tensor(self.Cout, self.Cin, Nb).double()
+        self.weights.requires_grad = True
 
         # biases
         # TODO: where to add bias? after the regular conv into every channel (Cout and S separately)?
+        #  -- Mark's tip: at the conv2d line
         # shapes of bias:
         self.biases = torch.Tensor()
+        self.biases.requires_grad = True
 
         self.init_params()
 
     def init_params(self, std=0.7071):
         self.weights = std * torch.randn_like(self.weights)
+        self.weights.requires_grad = True
         # TODO: std for biases?
         self.biases = torch.rand_like(self.biases)
+        self.biases.requires_grad = True
 
     def forward(self, x):
         """
@@ -67,7 +71,7 @@ class convTH:
 
         This translates to:
 
-        :param: x: input tensor which has a shape of [Cin, U, U] (channel-in, height, width)
+        :param: x: input tensor which has a shape of [N,Cin, U, U] (batch-size, channel-in, height, width)
 
         Returns:
             y: output tensor which has a shape of H - (Cout, S, U, U) where
@@ -78,7 +82,7 @@ class convTH:
         """
 
         # Extract dimensions
-        _, U, _ = x.shape()
+        N, C_in, U, _ = x.shape
 
         # TODO: autograd
         # TODO: add batch size
@@ -88,29 +92,37 @@ class convTH:
         # w:        [Cout, Cin, Nb]
         # filter:   [Cout, Cin, S, V, V ]
         # TODO: check if torch.matmul is the function for our case (see annotated figure 1)
-        filters = torch.matmul(self.weights, self.bases)
+        # TODO: cross-check this ein-sum
+        filters = torch.einsum('ijk, klmn -> ijlmn', self.weights, self.bases)
         # filters should be [Cout, Cin, S, V, V]
-        print(filters.shape)
+        check = (filters.shape == (self.Cout, self.Cin, self.S, self.V, self.V))
+        print(check)
 
         # expand
-        expanded_filters = filters.view(self.Cout, self.Cin * self.S, self.V, self.V)
-        # expanded_filters should be [Cout, Cin*S, V, V]
-        print(expanded_filters.shape)
+        # TODO: notify the authors that the paper might have a pretty significant 'bug' (I'm 95% sure):
+        # "...expand it to shape[Cout, CinS, V, V]. Then we use standard..." here it should be [CoutS, Cin, V, V]
+        expanded_filters = filters.view(self.Cout * self.S, self.Cin, self.V, self.V)
+        # expanded_filters should be [Cout, Cin*S, V, V] -> wrong, see above
+        check = (expanded_filters.shape == (self.Cout * self.S, self.Cin, self.V, self.V))
+        print(check)
 
         # convolve
         # TODO: bias?
-        # todo: stride, padding, ... ?
+        # todo: stride, padding, ... ? -- atm temporary fixed for retaining image shape w padding
         # TODO: is this the same as https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html ?
-        conv_output = torch.conv2d(x, expanded_filters)
-        # conv_output should be shaped [Cout, S, U, U] ... except when striding/padding is not normal
-        print(conv_output.shape)
+        conv_output = torch.conv2d(x, expanded_filters, padding=math.floor(self.V / 2))
+        # conv_output should be shaped [Cout * S, U, U] ... except when striding/padding is not normal
+        # plus batch-size -> [N, Cout * S, U, U]
+        check = (conv_output.shape == (N, self.Cout * self.S, U, U))
+        print(check)
 
         # squeeze
         # todo: stride, padding, ... - fix at U
         # todo: calculate U at runtime
-        y = conv_output.view(self.Cout, self.S, U, U)
+        y = conv_output.view(N, self.Cout, self.S, U, U)
         # y should be [Cout, S, U, U] ... expect when striding/padding...
-        print(y.shape)
+        check = (y.shape == (N, self.Cout, self.S, U, U))
+        print(check)
 
         return y
 
@@ -140,17 +152,60 @@ class convHH:
     pass
 
 
-def hermite_bases(x, n, y, m, A, sigma):
+def hermite_bases(Nb, S, V, A=1, sigma=5):
     """
+    :param: Nb: number of bases ... pay attention to the pyramid form (image below)
+            to get a complete layer of the pyramid, you need Nb=n(n+1)/2 where n is an element of N
+    :param: S: number of how many scales we will have
+    :param: V: x*y size of each base
+    :param: A: global scale factor for filter
+    :param: sigma: The sigma that's on the power of S
+                    sigma is the biggest scale, and exponentially converges to 1 in S steps
     basis
         -   2D Hermite polynomials with 2D Gaussian envelope,
         -   The basis is pre-calculated for all scales and fixed.
         -   For filters of size V × V , the basis is stored as an array of shape of [Nb, S, V, V ]
             -   Appendix C for more details
+
+        -   sigma seems to be the scale parameter -> corresponds to S
+        -   Nb seems to be the polynomial order -> n and m parameters?
+        -   see this pic: https://www.researchgate.net/publication/308494563/figure/fig1/AS:560765801566208@1510708393139/Two-dimensional-Hermite-TDH-functions-of-rank-0-to-7-in-A-polar-form-and-B.png
+        -   what on earth should A be?
     """
-    p1 = A / (sigma ** 2)
-    h1 = hermite(n, x / sigma)
-    h2 = hermite(m, y / sigma)
-    gaussian_envelope = math.exp(-((x ** 2 + y ** 2) / (2 * sigma ** 2)))
-    psi = p1 * h1 * h2 * gaussian_envelope
-    return psi
+    bases = torch.Tensor(Nb, S, V, V).double()
+    # determine n-m from Nb
+    # TODO: optimize this for loop
+    config_list = generate_hermite_list(Nb)
+    i = 0
+    for element in config_list:
+        n, m = element
+        # TODO: check if it's okay to run sigma like this
+        for s in range(S):
+            # sigma_inner runs from sigma ** 1 -> sigma ** (1/S)
+            # TODO: double check if sigma_inner is conceptually right
+            sigma_inner = sigma ** ((s + 1) ** -1)
+            x = np.linspace(int(math.ceil(-V / 2)), int(math.floor(V / 2)), V, dtype=np.double)
+            y = np.linspace(int(math.ceil(-V / 2)), int(math.floor(V / 2)), V, dtype=np.double)
+            p1 = A / (sigma_inner ** 2)
+            # note: using "physicist's Hermite polynomials" by using scipy
+            h1 = eval_hermite(n, x / sigma_inner)
+            h2 = eval_hermite(m, y / sigma_inner)
+            # TODO: double check if this is the gaussian?
+            #  ... might be the case that gaussian can be defined in multiple ways?? WTF??
+            gaussian_envelope = np.exp(-np.add.outer(x ** 2, y ** 2) / (2 * sigma_inner ** 2))
+            bases[i, s] = torch.from_numpy(p1 * np.outer(h1, h2) * gaussian_envelope)
+        i += 1
+
+    return bases
+
+
+def generate_hermite_list(Nb):
+    # this code is ugly as fuck
+    counter = 0
+    config = []
+    while True:
+        for i in range(counter + 1):
+            config.append((i, counter - i))
+            if len(config) == Nb:
+                return config
+        counter += 1
--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 from SESN_parts import convTH
+import numpy as np
+import torch
 
-cth = convTH()
+
+def dummy_image():
+    x_values = np.linspace(-10, 10, 50)
+    y_values = np.linspace(-10, 10, 50)
+    z_values = (1, 2, 3)
+    base_channel = np.add.outer(x_values, y_values)
+    img = np.multiply.outer(z_values, base_channel)
+    batch_maker = (1, 1)
+    img = np.multiply.outer(batch_maker, img)
+    return torch.from_numpy(img)
+
+
+cth = convTH(3, 2, 7, 5, 6)
+dummy_pic = dummy_image()
+layer2 = cth.forward(dummy_pic)
+print('requires grad:', cth.weights.requires_grad)
+
+external_grad = torch.rand_like(layer2)
+layer2.backward(gradient=external_grad)
+grads = cth.weights.grad
+print(grads.shape)
+print(grads)
+
+# TODO: move py files to src
